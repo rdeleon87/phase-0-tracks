c5f31e65937ee6fc92930137b6360e00f6284479
Robin De Leon <rdeleonrg@gmail.com>
2016-08-07 15:44:31 -0400

    adds methods to encrypt and decrypt

---
+++ ruby/secret_agents.rb
@@ -0,0 +1,39 @@
+# name our encryption method
+# for every letter of a string, advance to the next alphabetical letter 
+# if index contains a space, remain a space 
+
+def encrypt(password)
+	index = 0
+	while index < password.length
+		if password[index] == " " 
+			password[index] 
+		else 
+			password[index] = password[index].next!
+		end
+	    index += 1
+	end 
+	return password
+end
+
+# Decryption method:
+# for every letter in the string, go backwards one alphabetical letter
+# if index contains a space, remain a space
+
+def decrypt(password)
+	abc = "abcdefghijklmnopqrstuvwxyz"
+    index = 0
+	while index < password.length
+		if password[index] == " "
+		   password[index]
+		else
+			password[index] = abc[abc.index((password[index]))-1]
+		end
+		index += 1
+    end
+	return password 
+end
+
+#
+
+
+
