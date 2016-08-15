33f1d568e510fb32e82f7e4b0acc52726276aa1f
Robin De Leon <rdeleonrg@gmail.com>
2016-08-15 13:16:56 -0400

    adds hashes and arrys for rock show in brooklyn

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,37 @@
+Rock show in brooklyn
+
+stages = {
+    first_stage: {
+        band: 'Metallica',
+        seating_capacity: {
+            total_seats: 50000,
+            seats_available: 200
+        },
+        amps: [
+            "Mesa/Boogie dual rectifiers", 
+            "Marshall JVM"
+        ]
+    },
+    second_stage: {
+        band: 'Alice in chains',
+        seating_capacity: { 
+            total_seats: 30000,
+            seats_available: 0
+        },
+       amps: [
+            "Marshall JCM 800", 
+            "VHT pitbull"
+        ]
+    },
+    third_stage: {
+        band: 'Guns and Roses',
+        seating_capacity: {
+            total_seats: 60000,
+            seats_available: 0
+        },
+        amps: [
+            "Marshall Slash signature"
+            "Axe Fx"   
+        ]
+    }
+}
