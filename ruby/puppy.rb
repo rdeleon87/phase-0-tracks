ac9390375302f21a4ae235e03bb9c37ce895ce8f
Robin De Leon <rdeleonrg@gmail.com>
2016-08-21 14:27:53 -0400

    add class and call method on class

---
+++ ruby/puppy.rb
@@ -0,0 +1,21 @@
+# SPECIES ------------------------
+# Puppy
+
+
+# CHARACTERISTICS ----------------
+# Size: 20lbs
+# Friendly: yes
+# Sex: male/female
+# Name: Rocky
+# Color: light brown
+
+# BEHAVIOR -----------------------
+# Bark
+# jump
+# run
+
+
+class Puppy
+end
+
+rocky = Puppy.new
