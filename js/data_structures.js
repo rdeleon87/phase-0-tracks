8e32d717179024181c84da536c49665330587d7b
Robin De Leon <rdeleonrg@gmail.com>
2016-08-28 14:56:23 -0400

    adds lets build a car funtion

--- js/data_structures.js
+++ js/data_structures.js
@@ -15,4 +15,21 @@ function Car(make, model, year){
 this.make = make;
 this.model = model;
 this.year = year;
+
+this.tires = function() { console.log("Spin out!"); };
 }
+
+console.log("Let's build a car!");
+var new_car = new Car("toyota", "corolla", 2017);
+
+console.log(new_car);
+new_car.tires();
+
+var new_car2 = new Car("Honda", "civic", 1982);
+console.log(new_car2);
+new_car2.tires();
+
+var new_car3 = new Car("Tesla", "mark 1", 2014)
+console.log(new_car3);
+new_car3.tires();
+
